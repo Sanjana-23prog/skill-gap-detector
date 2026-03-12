ccf5c543b0186bf19af50910bdb3908e87679095
Sanjana-23prog <gampalavimasanjana@gmail.com>
2026-03-12 16:29:43 +0530

    Update firebase_config.py

--- database/firebase_config.py
+++ database/firebase_config.py
@@ -1,1 +1,11 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
 
+def initialize_firebase():
+    if not firebase_admin._apps:
+        cred = credentials.Certificate("database/firebase-key.json")
+        firebase_admin.initialize_app(cred)
+
+    db = firestore.client()
+    return db
