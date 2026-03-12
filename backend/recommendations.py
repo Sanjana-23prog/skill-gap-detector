45aef27ab4d8604e4118a85ff403c10c84eb8804
sowjzanya7411 <sowjzanya7411@gmail.com>
2026-03-12 16:08:14 +0530

    Update recommendations.py

--- backend/recommendations.py
+++ backend/recommendations.py
@@ -1,1 +1,14 @@
+import json
 
+def recommend_projects(missing_skills):
+
+    with open("data/projects.json") as f:
+        projects = json.load(f)
+
+    suggestions = []
+
+    for skill in missing_skills:
+        if skill.lower() in projects:
+            suggestions.extend(projects[skill.lower()])
+
+    return suggestions
