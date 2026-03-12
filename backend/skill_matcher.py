73bd5021e4e436c76391c46650f09b79924a8862
sowjzanya7411 <sowjzanya7411@gmail.com>
2026-03-13 00:13:28 +0530

    Update skill_matcher.py

--- backend/skill_matcher.py
+++ backend/skill_matcher.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+def find_missing_skills(user_skills, role):
+    
+    # load dataset
+    df = pd.read_csv("dataset.csv")
+
+    # get required skills for the role
+    role_data = df[["skill", role]]
+
+    required_skills = role_data["skill"].tolist()
+
+    missing_skills = []
+
+    for skill in required_skills:
+        if skill.lower() not in [s.lower() for s in user_skills]:
+            missing_skills.append(skill)
+
+    return missing_skills
